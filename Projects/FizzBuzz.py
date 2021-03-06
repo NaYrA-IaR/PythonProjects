5ed50e8997867d8f89fe42fdb01221d2793752c7
NaYrA-IaR <56719715+NaYrA-IaR@users.noreply.github.com>
2021-03-06 21:59:33 +0530

    FizzBuzz

---
+++ Projects/FizzBuzz.py
@@ -0,0 +1,12 @@
+print("a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.\n")
+
+for i in range(1,101):
+    if (i%3 == 0 and i%5 == 0):
+        print("FizzBuzz")
+    elif (i%5 == 0):
+        print("Buzz")
+    elif (i%3 == 0):
+        print("Fizz")
+    else:
+        print(i)
+print("\nThank You")
